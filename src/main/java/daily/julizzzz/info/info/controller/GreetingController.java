c0dbae0f3a37acf9af4cca35ba3fcfd478095abc
JulizzzZ <JulizzzZ@yeah.net>
2020-03-10 20:32:44 +0800

    server port modify

--- src/main/java/daily/julizzzz/info/info/controller/GreetingController.java
+++ src/main/java/daily/julizzzz/info/info/controller/GreetingController.java
@@ -1,4 +1,16 @@
 package daily.julizzzz.info.info.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class GreetingController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "greeting";
+    }
 }
